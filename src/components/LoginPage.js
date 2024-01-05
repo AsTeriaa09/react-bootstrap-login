045e2288e50abc2485448d091eb94e98433042f5
AsTeriaa09 <mahiraakhter950@gmail.com>
2024-01-05 09:10:49 +0600

    first commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "../Styles/Login.css";
+import { Link } from "react-router-dom";
+// import logo from "../images/image.png"
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 left-col col-12"></div>
+          <div className="col-lg-6 right-col col-12">
+            <form >
+              <div className="logo text-center">
+                <img src="/images/image.png" alt="" />
+              </div>
+
+              <div class="mb-5">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  class="form-control shadow-none"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  class="form-control shadow-none"
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <div className="forgot-pass  d-flex flex-row-reverse">
+                <Link to="#" className="link">
+                  <p>forgot password?</p>
+                </Link>
+              </div>
+
+              <button type="submit" class="btn my-3">
+                Login
+              </button>
+              <div className="sign-up">
+                <p>
+                  Already have an account?{" "}
+                  <Link to="#" className="link">
+                    Sign up
+                  </Link>{" "}
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
